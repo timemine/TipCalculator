41fb3e0d3b6fb78986c4f422141b7303dfaaffe9
timemine <timemine@hotmail.com>
2016-09-26 20:31:50 -0400

    Added functionality

--- ViewController.swift
+++ ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
@@ -17,10 +18,14 @@ class ViewController: UIViewController {
     
     //Global Variables
     let tipPercentages = [0.18, 0.2, 0.25]
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        keepThingsConstant()
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,33 +37,51 @@ class ViewController: UIViewController {
     //UI Control Events
     @IBAction func onTap(_ sender: AnyObject) {
         view.endEditing(true)
+        keepThingsConstant()
     }
     
     @IBAction func calculateTip(_ sender: AnyObject) {
         updateUI()
+        keepThingsConstant()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setDefaults()
         updateUI()
+        billField.text = String(defaults.double(forKey: "lastBillAmount"))
     }
     
     //Controller Functions
     func setDefaults(){
-        let defaults = UserDefaults.standard
         tipControl.selectedSegmentIndex = defaults.integer(forKey: "tipPercent")
+        keepThingsConstant()
+    }
+    
+    //Keep certain settings/values the same always
+    func keepThingsConstant(){
+        billField.becomeFirstResponder()
+        defaults.set(Double(billField.text!) ?? 0, forKey: "lastBillAmount")
     }
 
     func updateUI(){
-        
         let billAmount = Double(billField.text!) ?? 0
         let tip = billAmount * tipPercentages[tipControl.selectedSegmentIndex]
         let total = billAmount + tip
-        
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
-    }
 
+        tipLabel.text = formatToLocalCurrency(amount: tip)
+        totalLabel.text = formatToLocalCurrency(amount: total)
+    }
+    
+    
+    func formatToLocalCurrency(amount: Double) -> String{
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = NumberFormatter.Style.currency
+        // localize to your grouping and decimal separator
+        currencyFormatter.locale = Locale.current
+
+        return currencyFormatter.string(for: amount)!
+    }
 }
 
